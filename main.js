1d0059bcfdfd45bae107f985d6143a7c9181ca9d
RTdev <77839017+R1TGAMING@users.noreply.github.com>
2024-06-24 11:52:35 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function handleSubmit() {
+  const khodam = ["Bintang Skibidi", "Mewing", "Alomani", "Si Atmin", "Raja Sigma", "Mr. Ironi", "Mahkluk Jawa", "Ambatron", "Singa Emas", "Macan Putih", "Sosok Hytam", "Nugroho", "Mamang Rusdi", "Reimu Hakurei", "Cirno Jawa", "Onde Mande", "T-Rex Super Gyatt", "Knek", "Little Princess Si Dongo", "Go And Jo", "Mahkluk Komunis", "Si Kumis Penguasa", "Kucing Meow Meow", "Apeci Apecu", "Pace Yunus", "Toni 99 Kapolres", "Roblok Mewing", "Rehan Wangsaff", "Si Suki"]
+  const form = document.getElementById("form-khodam");
+  const result = document.getElementById("result")
+  const nama = document.getElementById("form-name").value;
+  const random = Math.floor(Math.random() * khodam.length)
+  
+  form.innerHTML = "<p>Selamat " + nama + " Khodam Mu Adalah <p>"+ "<br><h2><span class = khodamResult>"+khodam[random]+"</span><h2><br><h3>Jika Kalian Suka Support Terus Ipii Ya!!";
+  
+}
